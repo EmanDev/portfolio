138ecbff2a0502c667f8de7734dd4e8ab14fbbcf
Eman <game.and.windows.tutorial1@gmail.com>
2024-05-30 21:07:31 +0800

    Update 20240530
    - Added scroll progress bar
    - Remove unnecessary files
    - Check this commit for more info

--- assets/js/main.js
+++ assets/js/main.js
@@ -138,3 +138,13 @@ document.querySelectorAll('.modal-container').forEach(container => {
         }
     });
 });
+
+// scroll progress bar
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
